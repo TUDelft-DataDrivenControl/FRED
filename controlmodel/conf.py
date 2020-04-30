ba7550d33cef6b7ab9eb604993ba3e5f97af7baa
Maarten J. van den Broek <m.j.vandenbroek@tudelft.nl>
2020-04-30 14:35:13 +0200

    Add turbine parameter class

--- controlmodel/conf.py
+++ controlmodel/conf.py
@@ -11,8 +11,9 @@ class ControlModelParameters:
         self._config = None
 
     def load(self, file):
+        self._load_configuration_from_yaml(file)
         try:
-            self._load_configuration_from_yaml(file)
+           self._assign_configuration()
         except KeyError as ke:
             raise KeyError("Missing definition in config file, did not find {}".format(ke))
 
@@ -23,7 +24,23 @@ class ControlModelParameters:
     def print(self):
         print(yaml.dump(self._config))
 
+    def _assign_configuration(self):
+        self.turbine = self.Turbine(self._config["turbine"])
+
+    class Turbine:
+        """
+        Turbine configuration class
+        """
+        def __init__(self,config_dict):
+            self.axial_induction = config_dict["axial_induction"]
+            self.diameter = config_dict["diameter"]
+            self.radius = self.diameter / 2
+            self.thickness = config_dict["thickness"]
+            self.hub_height = config_dict["hub_height"]
+
+
 if __name__ == '__main__':
     par = ControlModelParameters()
     par.load("../config/test_config.yaml")
-    par.print()
+    # par.print()
+    # par.turbine.print()
