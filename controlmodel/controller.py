ac49a1bbec2d763d6ad877534a28c6591714d9fa
Maarten J. van den Broek <m.j.vandenbroek@tudelft.nl>
2020-05-01 11:59:36 +0200

    Update Controller class with switcher function

--- controlmodel/controller.py
+++ controlmodel/controller.py
@@ -8,14 +8,18 @@ class Controller:
         self._control_type = conf.par.wind_farm.controller.type
         self._wind_farm = wind_farm
         self._turbines = wind_farm.get_turbines()
-        self._yaw_ref = []
+        self._yaw_ref = conf.par.wind_farm.yaw_angles
         if self._control_type == "series":
             self._time_series = conf.par.wind_farm.controller.yaw_series[:,0]
             self._yaw_series = conf.par.wind_farm.controller.yaw_series[:,1:]
 
     def control_yaw(self, simulation_time):
-        self._fixed_yaw(simulation_time)
-        self._fixed_time_series(simulation_time)
+        switcher = {
+            "fixed": self._fixed_yaw,
+            "series": self._fixed_time_series
+        }
+        controller_function = switcher.get(self._control_type)
+        controller_function(simulation_time)
         self._update_yaw()
 
     def _fixed_yaw(self, simulation_time):
@@ -31,4 +35,7 @@ class Controller:
                 "Computed yaw reference (length {:d}) does not match {:d} turbines in farm."
                 .format(len(self._yaw_ref), len(self._turbines)))
 
-        [wt.set_yaw(y) for (wt,y) in zip(self._turbines, self._yaw_ref)]
+        for (wt, y) in zip(self._turbines, self._yaw_ref):
+            if wt.get_yaw() != y:
+                wt.set_yaw(y)
+        # [wt.set_yaw(y) for (wt,y) in zip(self._turbines, self._yaw_ref)]
