fb01add7a6aeac6814fca669ebfa2a51b15b2ed1
Maarten van den Broek <m.j.vandenbroek@tudelft.nl>
2021-02-17 15:39:44 +0100

    Set up FC for running in multiple processes

--- runfarmconners.py
+++ runfarmconners.py
@@ -1,18 +1,20 @@
 from fenics import *
 import controlmodel.conf as conf
-# if conf.with_adjoint:
-#     from fenics_adjoint import *
+if conf.with_adjoint:
+    from fenics_adjoint import *
 from controlmodel.windfarm import WindFarm
 from controlmodel.flowproblem import DynamicFlowProblem, SteadyFlowProblem
 from controlmodel.flowsolver import DynamicFlowSolver, SteadyFlowSolver
 import numpy as np
 import time
 
+from multiprocessing import Process
+
 import logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-16s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M',
-                    filename='farmcon.log',
+                    filename='./results/farmcon.log',
                     filemode='w')
 logger = logging.getLogger('')
 
@@ -21,17 +23,18 @@ parameters["form_compiler"]["optimize"] = True
 
 
 def main():
-    time_start = time.time()
-    # conf.par.load("config/farmconners/fc.A1.yaml")
-    # run_one_turbine_cases()
-    run_three_turbine_cases()
-    run_nine_turbine_cases()
+    with stop_annotating():
+        time_start = time.time()
+        # conf.par.load("config/farmconners/fc.A1.yaml")
+        # run_one_turbine_cases()
+        # run_three_turbine_cases() #12
+        run_nine_turbine_cases() #3
 
-    time_end = time.time()
-    logger.info("Total time: {:.2f} seconds".format(time_end - time_start))
+        time_end = time.time()
+        logger.info("Total time: {:.2f} seconds".format(time_end - time_start))
 
 def run_one_turbine_cases():
-    for yaw_offset in [0.]: #np.linspace(-30, 30, 7):
+    for yaw_offset in np.linspace(-30, 30, 7):
         conf.par.load("config/farmconners/fc.A4.1WT.yaml")
         conf.par.simulation.name = "fc.A4.1WT.Y{:03.0f}".format(yaw_offset)
         conf.par.wind_farm.yaw_angles += np.deg2rad(yaw_offset)
@@ -50,17 +53,22 @@ def run_three_turbine_cases():
                [-30., -10., 0.],
                [-30., -20., 0.],
                [-30., -30., 0.]]
-    for yaw_offsets in offsets[0:]:
+
+    def run_3WT_case(yaw_offsets):
         conf.par.load("config/farmconners/fc.A4.3WT.yaml")
         conf.par.simulation.name += ".Y{:03.0f}_Y{:03.0f}_Y{:03.0f}".format(*yaw_offsets)
         conf.par.wind_farm.yaw_angles += np.deg2rad(yaw_offsets)
         run_case()
 
+    for yaw_offsets in offsets[0:]:
+        Process(target=run_3WT_case,args=[yaw_offsets])
+
 def run_nine_turbine_cases():
     offsets = [[-10., -10., 0., -10., -20., 0., -10., -30., 0.],
                [-30., -10., 0., -30., -20., 0., -30., -30., 0.],
                [-10.,   0., 0., -20.,   0., 0., -30.,   0., 0.]]
-    for yaw_offsets in offsets[0:]:
+
+    def run_9WT_case(yaw_offsets):
         conf.par.load("config/farmconners/fc.A4.9WT.yaml")
         for row in range(3):
             connector = "." if row == 0 else "_"
@@ -72,6 +80,9 @@ def run_nine_turbine_cases():
                     *[np.abs(y/10.) for y in yaw_offsets[row::3]])
         conf.par.wind_farm.yaw_angles += np.deg2rad(yaw_offsets)
         run_case()
+
+    for yaw_offsets in offsets[0:]:
+        Process(target=run_9WT_case,args=[yaw_offsets]).start()
 
 def run_case():
     t0 = time.time()
